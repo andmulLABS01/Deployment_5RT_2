77d12a3c1f2b6d4db7804402d88a0efc6e6a58d7
Andrew Mullen <139258459+andmulLABS01@users.noreply.github.com>
2023-11-17 11:44:03 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 python3.7 -m venv test
 source test/bin/activate
-git clone https://github.com/kura-labs-org/c4_deployment-5.git
-cd c4_deployment-5
+git clone https://github.com/andmulLABS01/Deployment_5RT_2.git
+cd Deployment_5RT_2
 pip install -r requirements.txt
 pip install gunicorn
 python database.py
